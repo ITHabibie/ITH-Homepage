e9b0919916e65dc9958489fc2cb672be976a7b26
Ameeeek <ameeeekwork@gmail.com>
2024-02-13 21:30:27 +0800

    hapus komen

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -3,13 +3,13 @@
         @isset($contact)
         <div class="contact-info d-flex align-items-center">
             <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">{{$contact->email}}</a></i>
-            <!-- <i class="bi bi-phone d-flex align-items-center ms-4"><span>{{$contact->telepon}}</span></i> -->
+            <i class="bi bi-phone d-flex align-items-center ms-4"><span>{{$contact->telepon}}</span></i>
         </div>
         <div class="social-links d-none d-md-flex align-items-center">
-            <!-- <a href="{{$contact->twitter}}" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="{{$contact->twitter}}" class="twitter"><i class="bi bi-twitter"></i></a>
             <a href="{{$contact->facebook}}" class="facebook"><i class="bi bi-facebook"></i></a>
             <a href="{{$contact->instagram}}" class="instagram"><i class="bi bi-instagram"></i></a>
-            <a href="{{$contact->linkedin}}" class="linkedin"><i class="bi bi-linkedin"></i></a> -->
+            <a href="{{$contact->linkedin}}" class="linkedin"><i class="bi bi-linkedin"></i></a>
         </div>
         @endisset
     </div>
