519a8e4669a94712800deeee1b1dddb8636f1891
Ameeeek <ameeeekwork@gmail.com>
2024-02-18 10:15:37 +0800

    menambahkan konten pada navbar, next: perbaiki layout mobile pada nav dan top bar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,21 +1,20 @@
   <!-- ======= Header ======= -->
-  <header id="header" class="d-flex align-items-center">
+  <!-- <header id="header" class="d-flex align-items-center">
       <div class="container d-flex align-items-center justify-content-between">
           <h1 class="logo">
-              <img src="{{asset('assets/img/LogoITHH.svg')}}" style="width: 15rem; object-fit: cover">
-              <!-- <a href="{{route('homepage')}}">
+              <img src="{{asset('assets/img/LogoITHH.svg')}}" style="width: 15rem; object-fit: cover"> -->
+  <!-- <a href="{{route('homepage')}}">
             <img src="{{ asset('storage')}}/{{$contact->logo }}" style="max-height: 55px">
           </a> -->
-          </h1>
-          <!-- Uncomment below if you prefer to use an image logo -->
-          <!-- <a href="index.html" class="logo"><img src="{{asset('assets')}}/img/logo.png" alt=""></a> -->
-          <nav id="navbar" class="navbar">
-              <ul>
-                  <!-- <li><a class="nav-link {{ request()->is('/') ? 'active' : '' }}"  href="{{route('homepage')}}">{{$language == 'id' ? 'Beranda':'Home'}}</a></li> -->
-                  @foreach ($navbarmenu->whereIn('format',[0,1])->where('view','1')->sortBy('order') as $loop1)
+  <!-- </h1> -->
+  <!-- Uncomment below if you prefer to use an image logo -->
+  <!-- <a href="index.html" class="logo"><img src="{{asset('assets')}}/img/logo.png" alt=""></a> -->
+  <!-- <nav id="navbar" class="navbar">
+              <ul> -->
+  <!-- <li><a class="nav-link {{ request()->is('/') ? 'active' : '' }}"  href="{{route('homepage')}}">{{$language == 'id' ? 'Beranda':'Home'}}</a></li> -->
+  <!-- @foreach ($navbarmenu->whereIn('format',[0,1])->where('view','1')->sortBy('order') as $loop1)
                   @if ($loop1->format == 1)
                   <li class="dropdown title"><a href="#" class="{{ request()->is($loop1->slug.'/*') ? 'active' : '' }} title"><span>{{$language == 'id' ? $loop1->indtitle : $loop1->engtitle}}
-
                           </span> <i class="bi bi-chevron-down"></i></a>
                       <ul>
                           </p>
@@ -46,14 +45,14 @@
                       <a class="nav-link {{ request()->is($loop1->slug) ? 'active' : '' }}" href="{{ route('showNavs', [$loop1->slug]) }}">{{$language == 'id' ? $loop1->indtitle : $loop1->engtitle}}</a>
                   </li>
                   @endif
-                  @endforeach
-                  <!-- <li>
+                  @endforeach -->
+  <!-- <li>
                       <a class="nav-link" target="blank" href="{{ $pmb->url }}">P M B</a>
                   </li>
                   <li>
                       <a class="nav-link {{ request()->is('contacts') ? 'active' : '' }}" href="{{route('contacts')}}">{{__('cek.kontak-1')}}</a>
                   </li> -->
-                  <!-- <li class="dropdown">
+  <!-- <li class="dropdown">
               <a href="#">
                   <img class="rounded border border-secondary" src="{{$language == 'id' ? asset('assets/img/id.png'):asset('assets/img/en.png')}}" style="height: 15px;">
                   <i class="bi bi-chevron-down"></i>
@@ -71,13 +70,13 @@
                 </li>
             </ul>
           </li> -->
-              </ul>
-              <i class="bi bi-list mobile-nav-toggle"></i>
-          </nav><!-- .navbar -->
-      </div>
+  <!-- </ul> -->
+  <!-- <i class="bi bi-list mobile-nav-toggle"></i> -->
+  <!-- </nav>.navbar -->
+  <!-- </div> -->
 
 
-  </header> <!--End Header -->
+  <!-- </header> End Header -->
 
   <header id="header" class="d-flex align-items-center">
       <div class="container d-flex align-items-center justify-content-between">
@@ -88,50 +87,51 @@
                   @if ($loop1->format == 1)
                   <li class="submenu">
                       <a class="judul" href="">{{$language == 'id' ? $loop1->indtitle : $loop1->engtitle}}</a>
+<!-- 
                       <ul class="menu-container">
-                          <li class="menu-desc">
-                              {{$loop1->short_desc}}
-                          </li>
+                          <p>
+                              halo
+                          </p>
                           <div class="menu-opt">
-                              <ul>
+                              <ul class="grit">
+                                  <li>1</li>
+                                  <li>2</li>
+                                  <li>3</li>
+                                  <li>1</li>
+                                  <li>2</li>
+                                  <li>3</li>
+                                  <li>1</li>
+                                  <li>2</li>
+                                  <li>3</li>
+                                  <li>1</li>
+                                  <li>2</li>
+                                  <li>3</li>
+                              </ul>
+                          </div>
+                      </ul> -->
+
+                  <ul class="menu-container">
+                          <p style="font-size: 1rem;" class="menu-desc">
+                              {{$loop1->short_desc}}
+                          </p>
+                          <div class="menu-opt">
+                              <ul class="grit">
                                   @foreach ($navbarmenu->where('ops1',$loop1->id)->whereIn('format',[2,3])->where('view','1')->sortBy('order') as $loop2)
                                   @if ($loop2->format == 3)
-                                  <li>
-                                      {{$language == 'id' ? $loop2->indtitle : $loop2->engtitle}}
-                                  </li>
                                   @else
-                                  <ul>
-                                      <div class="kolom">
-                                          <!-- <a class="nav-link {{ request()->is($loop1->slug) ? 'active' : '' }}" href="{{ route('showNavs', [$loop1->slug]) }}">{{$language == 'id' ? $loop1->indtitle : $loop1->engtitle}}</a> -->
-                                          <li href="{{ route('showNav', [$loop1->slug, $loop2->slug]) }}">{{$language == 'id' ? $loop2->indtitle : $loop2->engtitle}}</li>
-
-                                          <!-- <li>halo</li> -->
-                                          <!-- <li>halo</li> -->
-                                          <!-- <li>halo</li> -->
-                                      </div>
-                                  </ul>
-                                  <!-- <a href="{{ route('showNav', [$loop1->slug, $loop2->slug]) }}">{{$language == 'id' ? $loop2->indtitle : $loop2->engtitle}}</a> -->
-
-                                  <!-- <a class="nav-link {{ request()->is($loop1->slug) ? 'active' : '' }}" href="{{ route('showNavs', [$loop1->slug]) }}">{{$language == 'id' ? $loop1->indtitle : $loop1->engtitle}}</a> -->
-
+                                  <li>
+                                      <a class="" href="{{ route('showNav', [$loop1->slug, $loop2->slug]) }}">{{$language == 'id' ? $loop2->indtitle : $loop2->engtitle}}</a>
+                                  </li>
                                   @endif
                                   @endforeach
                               </ul>
                           </div>
                       </ul>
                   </li>
-                  <!-- <li class="submenu">
-                      <a href="">halo</a>
-                      <ul>
-                          <li>ha</li>
-                          <li>he</li>
-                          <li>ho</li>
-                      </ul>
-                  </li> -->
                   @endif
                   @endforeach
               </ul>
-              <!-- <i class="bi bi-list mobile-nav-toggle"></i> -->
+              <i class="bi bi-list mobile-nav-toggle"></i>
           </nav>
       </div>
   </header>
