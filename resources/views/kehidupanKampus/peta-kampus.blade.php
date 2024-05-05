44b69abd1c034acfd9b76163fd6cf28b1eb01006
Ameeeek <ameeeekwork@gmail.com>
2024-05-06 07:19:43 +0800

    add peta kampus

--- resources/views/kehidupanKampus/peta-kampus.blade.php
+++ resources/views/kehidupanKampus/peta-kampus.blade.php
@@ -2,8 +2,38 @@
 
 @section('content')
  <section class="breadcrumbs">
-      <div class="container" style="margin-bottom: -1.2rem">
-        <div class="section-title">
-        </div>
+      <div class="container" style="margin-top: 11.2rem">
+        
+        <div class="row justify-content-center">
+          <div class="row col-lg-6">
+              <div class="col-md-6">
+                  <div class="info-box mb-4">
+                      <i class="bx bx-envelope"></i>
+                      <h3>{{__('cek.email')}}</h3>
+                      <p>{{$contact->email}}</p>
+                  </div>
+                  </div>
+
+                  <div class="col-md-6 mb-4">
+                  <div class="info-box ">
+                      <i class="bx bx-phone-call"></i>
+                      <h3>{{__('cek.telepon')}}</h3>
+                      <p>{{$contact->telepon}}</p>
+                  </div>
+                  </div>
+
+              <div class="col-lg-12 mb-4">
+                  <div class="info-box">
+                      <i class="bx bx-map"></i>
+                      <h3>{{__('cek.alamat')}}</h3>
+                      <p>{{$contact->alamat}}</p>
+                  </div>
+                  </div>
+          </div>
+          <div class="col-lg-6">
+              {!!$contact->maps!!}
+          </div>     
+        </div>  
+      </div>
     </section>
 @endsection
