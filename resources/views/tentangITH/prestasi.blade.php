d3d3042c687e650cbad948b661cb6f31746b75bd
Ameeeek <ameeeekwork@gmail.com>
2024-07-13 20:11:32 +0800

    fix prestasi

--- resources/views/tentangITH/prestasi.blade.php
+++ resources/views/tentangITH/prestasi.blade.php
@@ -1,28 +1,63 @@
 @extends('layouts.pages')
 @section('content')
-    <style>
-        @media(max-width:391px){
 
-            .hero-docs{margin-top:4rem;}
-            img{
-                width:8rem;
-                object-fit: cover;
-            }
-        }
-        @media (min-width:404px) and (max-width:508px){
-            .hero-docs{margin-top:9rem;}
-            img{
-                width:15rem;
-                object-fit: cover;
-            }
-        }
-
-        .deskripsi {
-            font-family: "Istok Web", sans-serif;
-            font-weight: 400;
-            font-style: normal;
+<style>
+  @media (max-width: 391px) {
+      .hero-docs { margin-top: 4rem; }
+      .col-md-3 {
+          width: 100%;
+          margin-right: 0;
+          margin-bottom: 1rem;
+          display: flex;
+          justify-content: center;
       }
-    </style>
+      img {
+          width: 100%;
+          object-fit: cover;
+          display: block;
+      }
+  }
+
+  @media (min-width: 392px) and (max-width: 768px) {
+      .hero-docs { margin-top: 6rem; }
+      .col-md-3 {
+          width: 100%;
+          margin-right: 0;
+          margin-bottom: 1rem;
+          display: flex;
+          justify-content: center;
+      }
+      img {
+          width: 100%;
+          object-fit: cover;
+          display: block;
+      }
+  }
+
+  @media (min-width: 769px) and (max-width: 991px) {
+      .hero-docs { margin-top: 6.2rem; }
+      .col-md-3 {
+          width: 50%;
+          margin-right: 0;
+          margin-bottom: 1rem;
+          display: flex;
+          justify-content: center;
+      }
+      img {
+          width: 100%;
+          object-fit: cover;
+          display: block;
+      }
+  }
+
+  .deskripsi {
+      font-family: "Istok Web", sans-serif;
+      font-weight: 400;
+      font-style: normal;
+  }
+</style>
+
+
    <div class="hero-docs text-center align" style="margin-top: 6.2rem; ">
         <div class="hero-text"
             style="background-color: rgba(0, 0, 0, 0.8);background-position:center;background-image:url({{ asset('assets/img/new_university.svg') }}); padding:2rem 1.5rem 2rem .5rem;color:white;">
@@ -34,15 +69,15 @@
     <main class="main-table d-flex" style="gap:2rem;">
         <div class="container">
             <div class="row">
-              <div class="col-3 me-5 mb-4">
+              <div class="col-md-3 ms-0 me-5 mb-4">
                 <div class="" style="width: 254px; height: 255px;">
-                    <img src="{{ asset('assets/img/prestasi/Gambar WhatsApp 2024-06-16 pukul 09.17.27_a2c3fba2 1.png') }}" class="card-img-top" alt="...">
+                    <img src="{{ asset('assets/img/prestasi/Gambar WhatsApp 2024-06-16 pukul 09.17.27_a2c3fba2 1.png')}}" class="card-img-top" alt="...">
                     <div class="card-body text-start p-0">
                       <p class="deskripsi card-text fw-bold mt-1">JUARA 2 AJANG LOMBA CRC (CELEBES ROBOT CONTEST) TAHUN 2024</p>
                     </div>
                   </div>
               </div>
-              <div class="col-3 me-5 mb-4">
+              <div class="col-md-3 me-5 ms-2 mb-4">
                 <div class="" style="width: 254px; height: 255px;">
                     <img src="{{ asset('assets/img/prestasi/Gambar WhatsApp 2024-06-16 pukul 23.32.01_c37a629d 1.png') }}" class="card-img-top" alt="...">
                     <div class="card-body text-start p-0">
@@ -50,7 +85,7 @@
                     </div>
                   </div>
               </div>
-              <div class="col-3 me-5 mb-4">
+              <div class="col-md-3 me-5 ms-2 mb-4">
                 <div class="" style="width: 254px; height: 255px;">
                     <img src="{{ asset('assets/img/prestasi/Gambar WhatsApp 2024-06-16 pukul 09.17.33_326f0ad9 1.png') }}" class="card-img-top" alt="...">
                     <div class="card-body text-start p-0">
@@ -60,7 +95,7 @@
               </div>
             </div>
             <div class="row">
-              <div class="col-3 me-5 mb-4">
+              <div class="col-md-3 me-5 ms-2 mb-4">
                 <div class="" style="width: 254px; height: 255px;">
                     <img src="{{ asset('assets/img/prestasi/Gambar WhatsApp 2024-06-16 pukul 23.33.00_d506b02e 1.png') }}" class="card-img-top" alt="...">
                     <div class="card-body text-start p-0">
@@ -68,7 +103,7 @@
                     </div>
                   </div>
               </div>
-              <div class="col-3 me-5 mb-4">
+              <div class="col-md-3 me-5 ms-2 mb-4">
                 <div class="" style="width: 254px; height: 255px;">
                     <img src="{{ asset('assets/img/prestasi/Gambar WhatsApp 2024-06-16 pukul 23.36.29_f42f2c46 1.png') }}" class="card-img-top" alt="...">
                     <div class="card-body text-start p-0">
